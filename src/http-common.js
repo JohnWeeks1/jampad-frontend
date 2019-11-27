6e24279965c128d1f33d473e834eb9a7ab5fd1b6
JohnWeeks1 <jonathanweeks007@gmail.com>
2019-11-27 22:24:03 +0000

    Refactored code and put token in to main.js

--- src/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import store from "./store";
-
-export const HTTP = axios.create({
-    baseURL: `http://jampad.localhost/api/`,
-    headers: {
-        Authorization: 'Bearer '+store.state.user.token,
-    }
-})
